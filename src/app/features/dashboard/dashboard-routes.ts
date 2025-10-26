2e02d26b0c155363c7f4da1876f240246f044ac5
manuNamuncura <manu.namun94@gmail.com>
2025-10-25 22:50:00 -0300

    feat: mejoras en autenticación y dashboard

--- src/app/features/dashboard/dashboard-routes.ts
+++ src/app/features/dashboard/dashboard-routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard';
 import { authGuard } from '../../shared/guards/auth.guard';
+import { UserRole } from '../auth/models/user.model';
 
 export const DashboardRoutes: Routes = [
     {
         path: '',
         component: DashboardComponent,
-        //canActivate: [authGuard],
-        data: { title: 'Dashboard' }
+        canActivate: [authGuard],
+        data: { 
+            roles: (UserRole.ADMIN),
+            title: 'Dashboard'
+        }
     }
 ]
